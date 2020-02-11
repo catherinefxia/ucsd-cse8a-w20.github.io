097149fa65fa82e18b560f50f05512bd6de5531d
Joe Gibbs Politz <joe.politz@gmail.com>
2020-02-10 19:52:51 -0800

    ,

--- lectures/CSE8AW20-02-06-Lec10-Arrays-All-Day/ArrayExamples.java
+++ lectures/CSE8AW20-02-06-Lec10-Arrays-All-Day/ArrayExamples.java
@@ -9,8 +9,20 @@ class ArrayExamples {
     int[] maybeCopy = nums;
     maybeCopy[2] = 999;
     System.out.println("Did nums[2] change?" + nums[2]);
+
     System.out.println(nums);
     System.out.println(maybeCopy);
+
+    int[] anotherArray = {1, 2, 3};
+    int[] anotherArray2 = {1, 2, 3};
+    anotherArray[0] = 999;
+    System.out.println(anotherArray2[0]);
+
+    String[] letters = {"a", "b", "c"};
+    String[] lettersAgain = {letters[0], "b", "c"};
+    letters[0] = "Z";
+    System.out.println(letters[0] + "\t" + lettersAgain[0]);
+
   }
 }
 
--- lectures/CSE8AW20-02-06-Lec10-Arrays-All-Day/Sounds.java
+++ lectures/CSE8AW20-02-06-Lec10-Arrays-All-Day/Sounds.java
@@ -1,12 +1,14 @@
 public class Sounds {
 
-    public static void main(String[] args){
-      int[] sound = CSE8ALib.readSound("sounds/UpbeatFunk.wav");
-      CSE8ALib.explore(sound);
+  public static void main(String[] args){
+    int[] sound = CSE8ALib.readSound("sounds/UpbeatFunk.wav");
 
-      for(int i = 0; i < 1000; i += 1) {
-        System.out.print(sound[i] + " ");
-      }
+    // CSE8ALib.play(sound);
+
+    System.out.println(sound.length / 22050.0);
+
+    for(int i = 0; i < 1000; i += 1) {
+      //System.out.print(sound[i] + " ");
     }
-
+  }
 }
